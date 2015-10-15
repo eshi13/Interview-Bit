8220e0ef0d023d1c2297e37f9855680972b00644
katlew <kat.lew.junk@gmail.com>
2015-10-14 22:54:39 -0400

    10/14 - spiral1.py & spiral2.py

--- max_sum.py
+++ max_sum.py
@@ -4,7 +4,7 @@ class Solution:
     # @return an integer
     def maxSubArray(self, A):
         current_sum = max_sum = A[0]
-        
+
     	for num in A[1:]:
             current_sum = max(num, current_sum+num)
             max_sum = max(current_sum, max_sum)
