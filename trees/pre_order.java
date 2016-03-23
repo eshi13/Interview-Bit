398ef54df9aa9d89251383bf14cdebb73b51c691
kathleenlew <klew@us.ibm.com>
2016-03-23 14:51:03 -0400

    3.23 - intersection_linked_list, preorder

---
+++ trees/pre_order.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	public ArrayList<Integer> preorderTraversal(TreeNode a) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<>();
+
+		if (A == null)
+			return res;
+		stack.push(A);
+		TreeNode node;
+
+		while (!stack.isEmpty()) {
+			
+		}
+	}
+}
