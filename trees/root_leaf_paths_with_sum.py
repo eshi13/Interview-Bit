ef5bd7402e91cba808b9a6e46066ef15bb1fae9a
katlew <kat.lew.junk@gmail.com>
2016-03-24 21:14:40 -0400

    3.24 - root_leaf_paths_sum.py

--- trees/root_leaf_paths_with_sum.py
+++ trees/root_leaf_paths_with_sum.py
@@ -6,15 +6,24 @@
 #         self.right = None
 
 class Solution:
-	# @param A : root node of tree
-	# @param B : integer
-	# @return an integer
-	def hasPathSum(self, A, B):
-		if A is None:
-			return 0
-		if A.val == B and A.left is None and A.right is None:
-			return 1	# successful
-		return self.hasPathSum(A.left, B-A.val) or self.hasPathSum(A.right, B-A.val)
+	def pathSum(self, root, num):
+		self.paths = []
+
+		if root.left:
+			self.checkPath(root.left, num, root.val, [root.val])
+		if root.right:
+			self.checkPath(root.right, num, root.val, [root.val])
+		return self.paths
+
+	def checkPath(self, node, num, current_val, path):
+		if num == current_val + node.val and node.left is None and node.right is None:
+			self.paths.append(path + [node.val])
+			return
+		if node.left:
+			self.checkPath(node.left, num, current_val + node.val, path + [node.val])
+		if node.right:
+			self.checkPath(node.right, num, current_val + node.val, path + [node.val])
+
 
 """
 Given a binary tree and a sum, find all root-to-leaf paths where each path’s sum equals the given sum.
