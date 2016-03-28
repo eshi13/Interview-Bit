5e0102c762f178c1c117b32644e274023e8eec69
kathleenlew <klew@us.ibm.com>
2016-03-28 16:16:09 -0400

    3.28 - recover_bst.java

---
+++ trees/next_right_pointers.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+    	Queue<TreeLinkNode> queue1 = new LinkedList<>();
+    	Queue<TreeLinkNode> queue2 = new LinkedList<>();
+
+    	if (root == null)	return;
+    	
+    }
+}
