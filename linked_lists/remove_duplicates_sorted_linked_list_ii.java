ea81e6ae6b6e6dd5af07316b7c8819941aa5af44
kathleenlew <klew@us.ibm.com>
2016-03-29 11:31:28 -0400

    3.29 - 1. remove_duplicates_ii

---
+++ linked_lists/remove_duplicates_sorted_linked_list_ii.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode root) {
+    	ListNode result = new ListNode(0);
+    	ListNode head = new ListNode(0);
+
+		while (root != null) {
+			if (root.next != null && root.val == root.next.val) {
+				int duplicate = root.val;
+				while (root != null && root.val == duplicate)
+					root = root.next;
+			}
+			else {
+				head.next = root;
+				head = head.next;
+				root = root.next;
+			}
+		}
+		head.next = null;
+		return result.next;
+    }
+}
--- linked_lists/remove_duplicates_sorted_linked_list_ii.py
+++ linked_lists/remove_duplicates_sorted_linked_list_ii.py
@@ -1,11 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
 	# @param A : head node of linked list
 	# @return the head node in the linked list
-	def deleteDuplicates(self, A):
-	   
+	def deleteDuplicates(self, root):
+		head = result = ListNode('dummy')
+
+		while root:
+			if root.next and root.val == root.next.val:
+				duplicate = root.val
+				while root and root.val == duplicate:
+					root = root.next
+			else:
+				head.next = root
+				head = head.next
+				root = root.next
+		head.next = None
+		return result.next
+"""
+Remove Duplicates from Sorted List IIBookmark
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+"""
