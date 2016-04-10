326f5967546a800a09ef482139e5d4e7e7635d57
katlew <kat.lew.junk@gmail.com>
2016-04-10 11:12:15 -0400

    4.10 - str_str.py

--- strings/str_str.py
+++ strings/str_str.py
@@ -1,10 +1,14 @@
-# Concept is there, but has error in Solution
+# for i, for j, haystack[i+j] != needle[j], isMatch = false
 
 class Solution:
 	# @param haystack : string
 	# @param needle : string
 	# @return an integer
 	def strStr(self, haystack, needle):
+		if len(haystack) == 0 or len(needle) == 0:
+			return -1
+		if len(needle) > len(haystack):
+			return -1
 		for i in xrange(len(haystack)):
 			isMatched = True
 			for j in xrange(len(needle)):
