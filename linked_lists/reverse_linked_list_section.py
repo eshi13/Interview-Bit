5f25703cf52f8739c51a71330ab13a99b2057d28
katlew <kat.lew.junk@gmail.com>
2016-04-11 20:38:18 -0400

    4.11 - reverse_linked_list

--- linked_lists/reverse_linked_list_section.py
+++ linked_lists/reverse_linked_list_section.py
@@ -1,3 +1,5 @@
+# find start pos, find prev, start reversing
+# temp = prev, temp.next = prev, move to next nodes
 class Solution:
     # func: reverse a linked list from position m to n
     # @param head: head node of the list
@@ -18,12 +20,12 @@ class Solution:
      
         #Start reversing
         prev = prev.next
-        curr = prev.next
+        temp = prev.next
         while count < n:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
             count += 1
      
         start.next = prev
