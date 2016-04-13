3bb886794c48dac5cd4c4863ca58156a79cd9027
katlew <kat.lew.junk@gmail.com>
2016-04-12 21:02:03 -0400

    4.12 - sudou

--- backtracking/subset_ii.py
+++ backtracking/subset_ii.py
@@ -1,3 +1,4 @@
+# set
 class Solution:
 	# @param A : list of integers
 	# @return a list of list of integers
@@ -11,3 +12,23 @@ class Solution:
 					mySet.add(x+[e])
 					res += x+[e]
 		return sorted(res)
+"""
+
+Given a collection of integers that might contain duplicates, S, return all possible subsets.
+
+ Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+The subsets must be sorted lexicographically.
+Example :
+If S = [1,2,2], the solution is:
+
+[
+[],
+[1],
+[1,2],
+[1,2,2],
+[2],
+[2, 2]
+]
+"""
