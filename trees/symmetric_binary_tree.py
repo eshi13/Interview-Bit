a63d42908ab9a5a4ceab7b30dc148d193b772880
kathleenlew <klew@us.ibm.com>
2016-04-21 16:40:52 -0700

    4.21 - isBalanced.py

---
+++ trees/symmetric_binary_tree.py
@@ -0,0 +1,33 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return an integer
+    def isSymmetric(self, A):
+
+"""
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+Example :
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+The above binary tree is symmetric. 
+But the following is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+"""
