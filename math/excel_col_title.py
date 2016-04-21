2c6f8392a56de53c51c1926553804029022cbaf8
kathleenlew <klew@us.ibm.com>
2016-04-21 14:42:35 -0700

    4.21 - excel_col_num.py

--- math/excel_col_title.py
+++ math/excel_col_title.py
@@ -1,18 +1,23 @@
 class Solution:
-    # @param A : integer
-    # @return a strings
-    def convertToTitle(self, A):
+	# @param A : integer
+	# @return a strings
+	def convertToTitle(self, A):
+		res = ""
+		while A >=0 :
+			res.append(str((A-1)/ 26))+1)
+			A %= 26
+		return res
 
 """
 Given a positive integer, return its corresponding column title as appear in an Excel sheet.
 
 For example:
 
-    1 -> A
-    2 -> B
-    3 -> C
-    ...
-    26 -> Z
-    27 -> AA
-    28 -> AB 
+	1 -> A
+	2 -> B
+	3 -> C
+	...
+	26 -> Z
+	27 -> AA
+	28 -> AB 
 """
