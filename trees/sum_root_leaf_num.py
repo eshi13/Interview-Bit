145a3379d101aa34d08366ed1f3b175c377693a0
kathleenlew <klew@us.ibm.com>
2016-05-05 15:38:07 -0400

    5.5 - sum_root_leaf_num

--- trees/sum_root_leaf_num.py
+++ trees/sum_root_leaf_num.py
@@ -10,7 +10,7 @@ class Solution:
 	# @return an integer
 	def sumNumbers(self, A):
 		return self.sumNum(A, 0)
-	def sumNum(A, currentSum):
+	def sumNum(self, A, currentSum):
 		if A is None:
 			return currentSum
 		currentSum = (currentSum * 10 + A.val) % 1003
@@ -20,5 +20,22 @@ class Solution:
 		if A.left is None:
 			return self.sumNum(A.right, currentSum)
 		if A.right is None:
-			reutrn self.sumNum(A.left, currentSum)
+			return self.sumNum(A.left, currentSum)
 		return (self.sumNum(A.left, currentSum) + self.sumNum(A.right, currentSum)) % 1003
+"""
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers % 1003.
+
+Example :
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = (12 + 13) % 1003 = 25 % 1003 = 25.
+"""
